fb0add85635d44bb145a3c1eab74860d9203b25c
SDuaFatima <160250491+SDuaFatima@users.noreply.github.com>
2024-05-29 08:05:46 -0700

    assignment:02

--- index.js
+++ index.js
@@ -3,4 +3,19 @@
 //such as, “Hello Eric, would you like to learn some Python today?”
 let personName = "Alex";
 console.log(`hello ${personName}, would you like to learn some Python today?`);
+//2: Name Cases: Store a person’s name in a variable, and then
+//print that person’s name in lowercase, uppercase, and titlecase.
+//this saves the name
+let firstName = "usman";
+let lastName = "ali";
+//shows the name in all small letters
+console.log(firstName.toLowerCase() + " " + lastName.toLowerCase());
+//shows the name in all capital letter
+console.log(firstName.toUpperCase() + " " + lastName.toUpperCase());
+//shows the first letter in capital
+console.log(firstName.charAt(0).toUpperCase() +
+    firstName.slice(1).toLowerCase() +
+    " " +
+    lastName.charAt(0).toUpperCase() +
+    lastName.slice(1).toLowerCase());
 export {};
--- index.ts
+++ index.ts
@@ -5,3 +5,24 @@
 let personName: string = "Alex";
 console.log(`hello ${personName}, would you like to learn some Python today?`);
 
+//2: Name Cases: Store a person’s name in a variable, and then
+//print that person’s name in lowercase, uppercase, and titlecase.
+
+//this saves the name
+let firstName: string = "usman";
+let lastName: string = "ali";
+
+//shows the name in all small letters
+console.log(firstName.toLowerCase() + " " + lastName.toLowerCase());
+
+//shows the name in all capital letter
+console.log(firstName.toUpperCase() + " " + lastName.toUpperCase());
+
+//shows the first letter in capital
+console.log(
+  firstName.charAt(0).toUpperCase() +
+    firstName.slice(1).toLowerCase() +
+    " " +
+    lastName.charAt(0).toUpperCase() +
+    lastName.slice(1).toLowerCase()
+);
