57513eb37b300c16d6a4859ed07c67199dd4a003
SDuaFatima <160250491+SDuaFatima@users.noreply.github.com>
2024-05-29 08:52:16 -0700

    assignment:05

--- index.js
+++ index.js
@@ -23,10 +23,17 @@ console.log(firstName.charAt(0).toUpperCase() +
 //look something like the following, including the quotation marks:
 // Albert Einstein once said, “A person who never made a mistake never tried anything new.”
 console.log(`Albert Einstein once said, “A person who never made a mistake never tried anything new.” `);
-//Famous Quote 2: Repeat Exercise 4, but this time store the famous
+//4: Famous Quote 2: Repeat Exercise 4, but this time store the famous
 //person’s name in a variable called famous_person. Then compose your
 //message and store it in a new variable called message. Print your message.
 let famous_person = "Albert Einstein";
 let message = `${famous_person} once said, “A person who never made a mistake never tried anything new.”`;
 console.log(message);
+//5: Stripping Names: Store a person’s name, and include some whitespace
+//characters at the beginning and end of the name. Make sure you use each
+//character combination, "\t" and "\n", at least once. Print the name once,
+//so the whitespace around the name is displayed. Then print the name after striping the white spaces.
+let name = "\t\n  Alex  \t\n"; //this saves the name whit whitespaces
+console.log(name); //print the name with whitespaces
+console.log(name.trim()); //print the name without whitspaces
 export {};
